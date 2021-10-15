78c4e1bbd10928d69bc273d3434c8cea33ac2ec8
UnknownCaz <unicornprincess522@gmail.com>
2021-10-14 22:49:04 -0400

    Fixed my noodle code and the .env files and similar stuff

--- my_bot.py
+++ my_bot.py
@@ -1,22 +1,27 @@
 import discord
 import random
 import os
+import dotenv
+from dotenv import load_dotenv
+load_dotenv("environ.env")
 
-#commands = ["$roll","$stats","$help"]
-
+# commands = ["$roll","$stats","$help"]
 client = discord.Client()
+
+
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print("We have logged in as {0.user}".format(client))
 
-def stat_roll(loops ,times, sides):
+
+def stat_roll(loops, times, sides):
     total = 0
     totals = []
     response = []
-    for r in range(1,loops+1):
+    for r in range(1, loops + 1):
         numbers = []
-        for i in range(1,times+1):
-            number = random.randint(1,sides)
+        for i in range(1, times + 1):
+            number = random.randint(1, sides)
             numbers.append(number)
         numbers = sorted(numbers)
         numbers.pop(0)
@@ -29,32 +34,36 @@ def stat_roll(loops ,times, sides):
     print(response)
     return response
 
-def roll_roll(dice,sides):
+
+def roll_roll(dice, sides):
     total = 0
     numbers = []
-    for i in range(1,int(dice)+1):
-        number = random.randint(1,int(sides))
+    for i in range(1, int(dice) + 1):
+        number = random.randint(1, int(sides))
         numbers.append(number)
         total += number
     return [numbers, total]
 
+
 @client.event
 async def on_message(message):
-    #print(message.content)
+    # print(message.content)
     if message.author == client.user:
         return
     else:
-        command = message.content.lower().replace("d"," ").split(" ")
-        if message.content.lower().replace("d"," ").split(" ")[0] == "$roll":
+        command = message.content.lower().replace("d", " ").split(" ")
+        if message.content.lower().replace("d", " ").split(" ")[0] == "$roll":
             print(command)
-            roll = roll_roll(command[1],command[2])
-            await message.channel.send("You rolled {0}, the sum is {1}".format(roll[0],roll[1]))
-        if(message.content.startswith('$stats')):
+            roll = roll_roll(command[1], command[2])
+            await message.channel.send(
+                "You rolled {0}, the sum is {1}".format(roll[0], roll[1])
+            )
+        if message.content.startswith("$stats"):
             com = message.content.split(" ")
             d1 = int(com[1])
             d2 = int(com[2])
             d3 = int(com[3])
-            await message.channel.send(stat_roll(d1,d2,d3))
+            await message.channel.send(stat_roll(d1, d2, d3))
 
-client.run(os.environ['BOT_KEY'])
 
+client.run(os.environ["BOT_KEY"])
